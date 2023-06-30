2230c22fdd67404aeeccf285ec279a4a306cb179
Dell <kaviprajapati2003@gmail.com>
2023-06-30 17:43:49 +0530

    Add project files.

---
+++ Class.cs
@@ -0,0 +1,14 @@
+﻿namespace JWT_Token_Demo
+{
+    public class Class
+    {
+        public Class(string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
+
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Controllers/AuthController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace JWT_Token_Demo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {    
+            IConfiguration configuration;
+            public AuthController(IConfiguration configuration)
+            {
+                this.configuration = configuration;
+            }
+
+            [AllowAnonymous]
+            [HttpPost]
+            public IActionResult Auth([FromBody] Class user)
+            {
+                IActionResult response = Unauthorized();
+                if (user != null)
+                {
+                    if (user.Username.Equals("sanukavisha@gmail.com") && user.Password.Equals("287"))
+                    {
+                        var issuer = configuration["Jwt:Issuer"];
+                        var audience = configuration["Jwt:Audience"];
+                        var key = Encoding.UTF8.GetBytes(configuration["Jwt:Key"]);
+                        var signingcredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha512Signature);
+
+
+                        var subject = new ClaimsIdentity(new[]
+                        {
+
+                    new Claim(JwtRegisteredClaimNames.Sub as string,user.Username),
+                    new Claim(JwtRegisteredClaimNames.Email as string,user.Username)
+
+
+                });
+                        var expires = DateTime.Now.AddMinutes(10);
+                        var TokenDescriptor = new SecurityTokenDescriptor
+                        {
+                            Subject = subject,
+                            Expires = expires,
+                            Issuer = issuer,
+                            Audience = audience,
+                            SigningCredentials = signingcredentials
+                        };
+                        var tokenHandler = new JwtSecurityTokenHandler();
+                        var token = tokenHandler.CreateToken(TokenDescriptor);
+                        var jwttoken = tokenHandler.WriteToken(token);
+                        return Ok(jwttoken);
+                    }
+                }
+                return response;
+            }
+        }
+}
